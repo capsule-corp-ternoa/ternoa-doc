49ad6bf2ee340ee32e3ce77fb4d4836d899adb8e
JulieChabanis <julie.chabanis07@gmail.com>
2022-09-28 17:41:12 +0200

    Add img require

---
+++ src/components/global/Toggle.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Toggle = (title, desc) => {
+  return (
+    <details>
+      <summary>{title}</summary>
+      <div>
+        <details>
+          <div>
+            🛸 Bi Bi Bop 🛸
+          </div>
+        </details>
+      </div>
+  </details>
+  )
+}
+
+export default Toggle
