f372a5bbbadb1b9e735f3428f5180b06946be594
JulieChabanis <julie.chabanis07@gmail.com>
2022-10-03 18:21:17 +0200

    Sidebar add border-left

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
       },
       docs: {
         sidebar: {
-          autoCollapseCategories: true, 
+          autoCollapseCategories: false, 
         }
       },
       footer: {
